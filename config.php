70188c0f68d57d21ad46f2aafc3c3cc8e8805fd6
weslleya <weslley.araujo2006@gmail.com>
2015-01-13 14:27:01 -0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,27 @@
 <?php
-require_once('functions.php');
-
-$btcBRL = getPrice('https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC');
-$btcFOXBIT = $btcBRL["high"];
-$btcFOXBIT = $btcBRL["low"];
-
-$btcFOXBIT = round($btcFOXBIT, 2);
-
+	require_once('functions.php');
+	
+	$cache = new Cache();
+	$btc_price = $cache->read('btc_price');
+	if (!$btc_price) {
+		$btcBRL = getPrice('https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC');
+		
+		$btcFOXBIT_high = $btcBRL["high"];
+		$btcFOXBIT_high = round($btcFOXBIT_high, 2);
+		
+		$btcFOXBIT_low = $btcBRL["low"];
+		$btcFOXBIT_low = round($btcFOXBIT_low, 2);
+		
+		$btcFOXBIT_vol = $btcBRL["vol"];
+		$btcFOXBIT_vol = round($btcFOXBIT_vol, 2);
+		
+		$btc_price = $btcFOXBIT_high."|".$btcFOXBIT_low."|".$btcFOXBIT_vol;
+		$cache->save('btc_price', $btc_price, '10 minutes');
+	}
+	
+	$btc_price = explode("|",$btc_price);
+	
+	$btcFOXBIT_high = $btc_price[0];
+	$btcFOXBIT_low = $btc_price[1];
+	$btcFOXBIT_vol = $btc_price[2];
 ?>
