4770065a6fddc13671d50002cb9c1685f78a0579
weslleya <weslley.araujo2006@gmail.com>
2015-01-13 14:27:27 -0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
-// Decodifica JSON
-function getPrice($url) {
+	require_once "class.php";
+	
+	function getPrice($url) {
 		$decode = @file_get_contents($url);
 		return json_decode($decode, true);
-}
+	}
 ?>
