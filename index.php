906fa570d20c5044c2bd075b88b45ab929613764
André Panzetti <andre@panzetti.com.br>
2015-01-14 14:05:13 -0300

    .
    .

--- index.php
+++ index.php
@@ -5,89 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>BTC-WIDGET</title>
     <link href='http://fonts.googleapis.com/css?family=Sintony' rel='stylesheet' type='text/css'>
-    <style>
-		.container {
-			width:250px;
-			height:270px;
-			background-color:#E6E6E6;
-			font-family: 'Sintony', sans-serif;
-			border-radius:15px;
-    		-moz-border-radius:15px;
-    		-webkit-border-radius:15px;
-			border: 1px solid;
-		}
-		.container .price {
-			width:250px;
-			height:110px;
-		}
-		.container .price #header {
-			width:250px;
-			height:30px;
-			background-color:#777;
-			background-image:-webkit-gradient(linear, left top, left bottom, from(#777), to(#555));
-			background-image:-webkit-linear-gradient(top, #777, #555);
-			background-image:-moz-linear-gradient(top, #777, #555);
-			background-image:-ms-linear-gradient(top, #777, #555);
-			background-image:-o-linear-gradient(top, #777, #555);
-			background-image:linear-gradient(top, #777, #555);
-			border-radius:15px 15px 0px 0px;
-			text-align:center;
-			font-size:18px;
-			color:#FFFFFF;
-			font-weight:bold;
-		}
-		.container .price #title {
-			width:90px;
-			height:60px;
-			float:left;
-			margin:15px 15px 15px 15px;
-			font-size:18px;
-			font-weight:bold;
-		}
-		.container .price #title #bitcoin {
-			margin:0;
-			width:150px;
-			float:left;
-		}
-		.container .price #title #by {
-			margin:0 0 0 12px;
-			width:20px;
-			float:left;
-		}
-		.container .price #title img {
-			margin-top:5px;
-		}
-		.container .price .value {
-			width:115px;
-			height:60px;
-			float:right;
-			font-size:14px;
-			margin:15px 5px 15px 5px;
-		}
-		.container .price .value #item_value {
-			text-align:right;	
-		}
-		.container .news #header {
-			width:250px;
-			height:5px;
-			background-color:#777;
-			background-image:-webkit-gradient(linear, left top, left bottom, from(#777), to(#555));
-			background-image:-webkit-linear-gradient(top, #777, #555);
-			background-image:-moz-linear-gradient(top, #777, #555);
-			background-image:-ms-linear-gradient(top, #777, #555);
-			background-image:-o-linear-gradient(top, #777, #555);
-			background-image:linear-gradient(top, #777, #555);
-			text-align:center;
-			font-size:18px;
-			color:#FFFFFF;
-			font-weight:bold;
-		}
-		.container .news #item_news {
-			width:250px;
-			font-size:12px;
-			margin:5px;
-		}
-	</style>
+	<link href="css/btc.css" rel="stylesheet">
 </head>
 <body>
 	<div class="container">
@@ -102,11 +20,12 @@
         </div>
         <div class="news">
         	<div id="header"></div>
-            <?php
+            <ul>
+			<?php
 				$count=0;
 				foreach($rss->url as $entrada) {
 					$title=explode("/",$entrada->loc);
-					echo '<div id="item_news"><a href="'.$entrada->loc.'">'.str_replace("-"," ",$title[4]).'</a></div>';
+					echo '<div id="item_news"><li><a href="'.$entrada->loc.'">'.str_replace("-"," ",$title[4]).'</a></li></div>';
 					if($count==3){
 						break;
 					} else {
@@ -114,6 +33,7 @@
 					}
 				}
 			?>
+			</ul>
         </div>
     </div>
 </body>
